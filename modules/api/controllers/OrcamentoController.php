93392a3d2c34ec69452af29bc97d86ac7a996526
Robson Monteiro Serrao~ <a61196@alunos.ipb.pt>
2024-05-14 21:38:41 +0100

    Retesting

--- modules/api/controllers/OrcamentoController.php
+++ modules/api/controllers/OrcamentoController.php
@@ -6,38 +6,7 @@ namespace app\modules\api\controllers;
 class OrcamentoController extends BaseRestController
 {
    public $modelClass = 'app\models\Orcamento';
-   namespace app\controllers;
 
-use Yii;
-use yii\rest\ActiveController;
-use yii\web\Response;
-
-class OrcamentosController extends ActiveController
-{
-    public $modelClass = 'app\models\Orcamentos';
-
-    // public function actions()
-    // {
-    //     $actions = parent::actions();
-    //     unset($actions['index']); // Desativar a action padrão de listagem de orçamentos
-    //     return $actions;
-    // }
-
-    // public function actionIndex()
-    // {
-    //     Yii::$app->response->format = Response::FORMAT_JSON;
-
-    //     // Sua consulta SQL para recuperar os orçamentos com informações dos estados
-    //     $sql = "
-    //         SELECT orcamento.*, orcamento AS nome_estado
-    //         FROM orcamento
-    //         LEFT JOIN estado ON orcamento.estado_id = estado.id
-    //     ";
-
-    //     $orcamentos = Yii::$app->db->createCommand($sql)->queryAll();
-
-    //     return $orcamentos;
-    }
 }
 
 
