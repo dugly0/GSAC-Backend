4dbfb94c3a788a9c9d42c3323a1a44e019321fda
Gabriel Rosario <a58917@alunos.ipb.pt>
2024-05-15 15:35:24 +0100

    add-seguranca-endpoint-buscar-orcamento

--- models/User.php
+++ models/User.php
@@ -2,103 +2,130 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property int $role_id
+ * @property int $status
+ * @property string|null $email
+ * @property string|null $username
+ * @property string|null $password
+ * @property string|null $auth_key
+ * @property string|null $access_token
+ * @property string|null $logged_in_ip
+ * @property string|null $logged_in_at
+ * @property string|null $created_ip
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ * @property string|null $banned_at
+ * @property string|null $banned_reason
+ *
+ * @property Profile[] $profiles
+ * @property Role $role
+ * @property UserAuth[] $userAuths
+ * @property UserToken[] $userTokens
+ * @property Utilizador[] $utilizadors
+ */
+class User extends \yii\db\ActiveRecord
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
-
     /**
      * {@inheritdoc}
      */
-    public static function findIdentity($id)
+    public static function tableName()
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return 'user';
     }
 
     /**
      * {@inheritdoc}
      */
-    public static function findIdentityByAccessToken($token, $type = null)
+    public function rules()
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return [
+            [['role_id', 'status'], 'required'],
+            [['role_id', 'status'], 'integer'],
+            [['logged_in_at', 'created_at', 'updated_at', 'banned_at'], 'safe'],
+            [['email', 'username', 'password', 'auth_key', 'access_token', 'logged_in_ip', 'created_ip', 'banned_reason'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+            [['username'], 'unique'],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
     }
 
     /**
-     * Finds user by username
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'role_id' => 'Role ID',
+            'status' => 'Status',
+            'email' => 'Email',
+            'username' => 'Username',
+            'password' => 'Password',
+            'auth_key' => 'Auth Key',
+            'access_token' => 'Access Token',
+            'logged_in_ip' => 'Logged In Ip',
+            'logged_in_at' => 'Logged In At',
+            'created_ip' => 'Created Ip',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'banned_at' => 'Banned At',
+            'banned_reason' => 'Banned Reason',
+        ];
+    }
+
+    /**
+     * Gets query for [[Profiles]].
      *
-     * @param string $username
-     * @return static|null
+     * @return \yii\db\ActiveQuery
      */
-    public static function findByUsername($username)
+    public function getProfiles()
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        return $this->hasMany(Profile::class, ['user_id' => 'id']);
     }
 
     /**
-     * {@inheritdoc}
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getAuthKey()
-    {
-        return $this->authKey;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validateAuthKey($authKey)
-    {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
+     * Gets query for [[Role]].
      *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
+     * @return \yii\db\ActiveQuery
      */
-    public function validatePassword($password)
+    public function getRole()
     {
-        return $this->password === $password;
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * Gets query for [[UserAuths]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserAuths()
+    {
+        return $this->hasMany(UserAuth::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[UserTokens]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserTokens()
+    {
+        return $this->hasMany(UserToken::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Utilizadors]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUtilizadors()
+    {
+        return $this->hasMany(Utilizador::class, ['user_id' => 'id']);
     }
 }
--- modules/api/controllers/OrcamentoController.php
+++ modules/api/controllers/OrcamentoController.php
@@ -5,6 +5,7 @@ namespace app\modules\api\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use app\models\Orcamento;
+use app\models\User;
 
 class OrcamentoController extends BaseRestController
 {
@@ -23,18 +24,26 @@ class OrcamentoController extends BaseRestController
     // Endpoint personalizado para retornar o orçamento com base no utilizador_id
     public function actionOrcamentoPorUtilizadorId($utilizador_id)
     {
-        $orcamentos = Orcamento::find()->where(['utilizador_id' => $utilizador_id])->all();
+        // Obter o token da autorização dos cabeçalhos da solicitação
+        $authorizationHeader = Yii::$app->getRequest()->getHeaders()->get('Authorization');
+        //corta a string para obter apenas o token
+        $token = str_replace('Bearer ', '', $authorizationHeader);
+        //busca o utilizador com o token fornecido
+        $user = User::find()->where(['access_token' => $token])->one();
+        if($utilizador_id == $user->id){
+            $orcamentos = Orcamento::find()->where(['utilizador_id' => $utilizador_id])->all();
 
-        if (empty($orcamentos)) {
-            throw new \yii\web\NotFoundHttpException("Não foram encontrados orçamentos para o utilizador com ID $utilizador_id.");
+            if (empty($orcamentos)) {
+                throw new \yii\web\NotFoundHttpException("Não foram encontrados orçamentos para o utilizador com ID $utilizador_id.");
+            }
+            return $orcamentos;
         }
-
-        return $orcamentos;
+        else{
+            throw new \yii\web\NotFoundHttpException("Voce não tem permissão para ver os orçamentos de outro utilizador.");
+        }
     }
     public function actionCreate()
     {
-        $requestData = Yii::$app->getRequest()->getBodyParams();
-        
         // Verificar se os campos necessários estão presentes no corpo da requisição
         $requiredFields = ['data_entrada', 'descricao', 'utilizador_id'];
         foreach ($requiredFields as $field) {
