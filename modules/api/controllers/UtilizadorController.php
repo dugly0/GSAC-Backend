bb36607855be6facfc107997990b03d1207773af
Gabriel Augusto Engelles Morais <a58790@alunos.ipb.pt>
2024-05-17 10:32:57 +0100

    utilizador

--- modules/api/controllers/UtilizadorController.php
+++ modules/api/controllers/UtilizadorController.php
@@ -2,10 +2,20 @@
 
 namespace app\modules\api\controllers;
 
+use yii\filters\auth\HttpBearerAuth;
 
 class UtilizadorController extends BaseRestController
 {
    public $modelClass = 'app\models\Utilizador';
+
+   public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::className(),
+        ];
+        return $behaviors;
+    }
 
 }
 
