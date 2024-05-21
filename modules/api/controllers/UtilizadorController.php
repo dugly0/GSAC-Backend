a835b8f552b042999ca3370b3ec92d052f5d2110
a58071 <a58071@alunos.ipb.pt>
2024-05-21 14:47:15 +0100

    end point view funciona para mostrar informações da tabela user/utilizador

--- modules/api/controllers/UtilizadorController.php
+++ modules/api/controllers/UtilizadorController.php
@@ -4,11 +4,47 @@ namespace app\modules\api\controllers;
 
 use amnah\yii2\user\models\User;
 use app\models\Utilizador;
-use yii\filters\auth\HttpBearerAuth;
 use yii\web\NotFoundHttpException;
 
 class UtilizadorController extends BaseRestController
 {
    public $modelClass = 'app\models\Utilizador';
+   
+   public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['view']);
+        unset($actions['index']);
+        return $actions;
+    }
+
+   public function actionView($id)
+{
+    $utilizador = $this->findModel($id);
+
+    if (!$utilizador) {
+        throw new NotFoundHttpException('Utilizador não encontrado.');
+    }
+
+    $user = User::findOne(['id' => $id]);
+
+    if (!$user) {
+        throw new NotFoundHttpException('Utilizador não encontrado.');
+    }
+
+    return [
+        'user' => $user,
+        'utilizador' => $utilizador,
+    ];
+   }
+
+   public function findModel($id)
+   {
+   $model = Utilizador::findOne($id);
+   if (!$model) {
+      throw new NotFoundHttpException('Utilizador não encontrado.');
+   }
+   return $model;
+   }  
 }
 ?>
