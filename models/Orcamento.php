3757b342babd94ff91eefab1c7d96b17f0dfaa2d
joao vitor <a58070@alunos.ipb.pt>
2024-05-22 16:50:31 +0100

    Comenta funções na model orcamento

--- models/Orcamento.php
+++ models/Orcamento.php
@@ -116,18 +116,24 @@ class Orcamento extends \yii\db\ActiveRecord
         return $this->hasOne(Utilizador::class, ['id' => 'utilizador_id']);
     }  
 // João
-    public function getEstadoOrcamentosAtivos()
-    {
-        return $this->hasMany(EstadoOrcamento::className(), ['orcamento_id' => 'id'])
-                    ->andOnCondition(['estado_orcamento.estado_id' => 1]); 
-    }
+    // public function getEstadoOrcamentosAtivos()
+    // {
+    //     return $this->hasMany(EstadoOrcamento::className(), ['orcamento_id' => 'id'])
+    //                 ->andOnCondition(['estado_orcamento.estado_id' => 1]); 
+    // }
 
-    public static function findOrcamentosComEstadoAtivo() 
-    {
-        return self::find()
-            ->joinWith('estadoOrcamentosAtivos')
-            ->where(['estado_orcamento.estado_id' => 1]);
-    }     
+    // public static function findOrcamentosComEstadoAtivo() 
+    // {
+    //     return self::find()
+    //         ->joinWith('estadoOrcamentosAtivos')
+    //         ->where(['estado_orcamento.estado_id' => 1]);
+    // }     
+
+    // public function getServicosAtivos() // Novo método
+    // {
+    //     return $this->hasMany(Servico::className(), ['id' => 'servico_id'])
+    //         ->viaTable('servico_orcamento', ['orcamento_id' => 'id']);
+    // }
 
     
 
