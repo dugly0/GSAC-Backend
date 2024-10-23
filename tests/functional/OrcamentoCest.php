f3ff6a0a87618c7109b5735a59a4cee56330092f
Gabriel Rosario <a58917@alunos.ipb.pt>
2024-10-23 20:41:50 +0100

    adicionando testes de orcamentos

--- tests/functional/OrcamentoCest.php
+++ tests/functional/OrcamentoCest.php
@@ -5,7 +5,8 @@ class OrcamentoCest
     // Propriedades da classe para armazenar os tokens
     public $token_user_comum = "O4YNJqGq103jkeBH7lmt82L5ByRVw1bn";
     public $token_user_admin = "LnkeNnnbkd3N5WZiYOR_9RE8k33nK1RX";
-    public $body = [
+    public $id = 1;
+    public $body_to_create = [
         "descricao"=> "dfsdffsd",
         "preco"=> 250,
         "data_entrega"=> "2024-05-20",      
@@ -28,65 +29,122 @@ class OrcamentoCest
                 ]
             ]            
         ];
-
-    public function _before(FunctionalTester $I)
-    {
-        // Executado antes de cada teste
-    }
-
-    public function _after(FunctionalTester $I)
-    {
-        // Executado após cada teste
-    }
-
-    // Teste da rota de login
+    public $body_to_update = [
+        "descricao"=> "dfsdffsd",
+        "preco"=> 250,
+        "data_entrega"=> "2024-05-20",  
+        "fatura"=> null,
+        "utilizador_id"=> 3,   
+        "laboratorio_id"=> 1,
+        "estadoOrcamentos"=>[
+            [
+                "estado_id"=> 5,
+                "data"=> "2024-04-13"
+            ]
+        ],
+        "servicoOrcamentos"=> [
+                [
+                    "servico_id"=> 6,
+                    "quantidade"=> 1
+                ]
+            ]            
+        ];
+    public $body_to_update_errado = [
+        "descricao"=> "dfsdffsd",
+        "preco"=> 250,
+        "data_entrega"=> "2024-05-20",  
+        "fatura"=> null,
+        "utilizador_id"=> 3,   
+        "laboratorio_id"=> 1,        
+        "servicoOrcamentos"=> [
+                [
+                    "servico_id"=> 6,
+                    "quantidade"=> 1
+                ]
+            ]            
+        ];
     public function testCreateOrcamento(FunctionalTester $I)
     {
-        // Define o header de autorização usando a propriedade da classe
         $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
 
-        $body = $this->body;
+        $body = $this->body_to_create;
         
-        // Envia uma requisição POST para a rota de orçamentos
-        $I->sendPOST('/api/orcamento', $body); // Não esqueça de incluir o corpo da requisição
+        $I->sendPOST('/api/orcamento', $body);
 
-        // Verifica se o código de resposta é 200 (OK)
         $I->seeResponseCodeIs(200);
 
-        // Verifica que a resposta é do tipo JSON
         $I->seeResponseIsJson();
     }
-
     public function testCreateOrcamentoSemAutorizacao(FunctionalTester $I)
     {
-        // Define o header de autorização usando a propriedade da classe
         $I->haveHttpHeader('Authorization', 'Bearer teste');
 
-        $body = $this->body;
+        $body = $this->body_to_create;
         
-        // Envia uma requisição POST para a rota de orçamentos
-        $I->sendPOST('/api/orcamento', $body); // Não esqueça de incluir o corpo da requisição
+        $I->sendPOST('/api/orcamento', $body);
 
-        // Verifica se o código de resposta é 200 (OK)
         $I->seeResponseCodeIs(401);
 
-        // Verifica que a resposta é do tipo JSON
         $I->seeResponseIsJson();
     }
     public function testCreateOrcamentoBodyErrado(FunctionalTester $I)
     {
-        // Define o header de autorização usando a propriedade da classe
         $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
 
         $body = $this->body_errado;
         
-        // Envia uma requisição POST para a rota de orçamentos
-        $I->sendPOST('/api/orcamento', $body); // Não esqueça de incluir o corpo da requisição
+        $I->sendPOST('/api/orcamento', $body);
 
-        // Verifica se o código de resposta é 200 (OK)
         $I->seeResponseCodeIs(400);
 
-        // Verifica que a resposta é do tipo JSON
+        $I->seeResponseIsJson();
+    }
+    public function testUpdateOrcamento(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+
+        $body = $this->body_to_update;
+        $id = $this->id;
+        $I->sendPUT('/api/orcamento/'. $id, $body);
+
+        $I->seeResponseCodeIs(200);
+
+        $I->seeResponseIsJson();
+    }
+    public function testUpdateOrcamentoSemAutorizacao(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer teste');
+
+        $body = $this->body_to_update;
+        $id = $this->id;
+        $I->sendPUT('/api/orcamento/'. $id, $body);
+
+        $I->seeResponseCodeIs(401);
+
+        $I->seeResponseIsJson();
+    }
+    public function testUpdateOrcamentoBodyErrado(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+
+        $body = $this->body_to_update_errado;
+        $id = $this->id;
+        $I->sendPUT('/api/orcamento/'. $id, $body);
+
+        $I->seeResponseCodeIs(401);
+
+        $I->seeResponseIsJson();
+    }
+    public function testUpdateOrcamentoIdErrado(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+
+        $body = $this->body_to_update;
+        $id = $this->id;
+        $I->sendPUT('/api/orcamento/30', $body);
+
+        $I->seeResponseCodeIs(404);
+
         $I->seeResponseIsJson();
     }
 }
