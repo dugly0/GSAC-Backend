86b4264981dbcdee00555836c44356b131910352
Gabriel Rosario <a58917@alunos.ipb.pt>
2024-10-26 15:31:02 +0100

    testes no endpoint orcamento

--- tests/functional/OrcamentoCest.php
+++ tests/functional/OrcamentoCest.php
@@ -3,46 +3,38 @@
 class OrcamentoCest
 {
     // Propriedades da classe para armazenar os tokens
-    public $token_user_comum = "O4YNJqGq103jkeBH7lmt82L5ByRVw1bn";
-    public $token_user_admin = "LnkeNnnbkd3N5WZiYOR_9RE8k33nK1RX";
+    public $token_user_comum = "9_hQg4sDkXdPpNS2PoaZwWVgIoppAzXZ";
+    public $token_user_admin = "wK9DpRsBpCPjxyBTzkJ1fBU2-pbpaTyq";
+    public $token_user_laboratorio = "-e4Co38nAcu9b08vtfHVhV64Aqllqzq9";
     public $id = 1;
     public $body_to_create = [
         "descricao"=> "dfsdffsd",
         "preco"=> 250,
         "data_entrega"=> "2024-05-20",      
         "laboratorio_id"=> 1,
-        "servico_orcamento"=> [
+        "servicoOrcamento"=> [
                 [
                     "servico_id"=> 6,
                     "quantidade"=> 1
                 ]
             ]            
         ];
-    public $body_errado = [
+    public $body_errado_to_create = [
         "preco"=> 250,
         "data_entrega"=> "2024-05-20",      
-        "laboratorio_id"=> 1,
-        "servico_orcamento"=> [
-                [
-                    "servico_id"=> 6,
-                    "quantidade"=> 1
-                ]
-            ]            
+        "laboratorio_id"=> 1                 
         ];
     public $body_to_update = [
         "descricao"=> "dfsdffsd",
         "preco"=> 250,
         "data_entrega"=> "2024-05-20",  
-        "fatura"=> null,
-        "utilizador_id"=> 3,   
         "laboratorio_id"=> 1,
-        "estadoOrcamentos"=>[
+        "estadoOrcamento"=>[
             [
                 "estado_id"=> 5,
-                "data"=> "2024-04-13"
             ]
         ],
-        "servicoOrcamentos"=> [
+        "servicoOrcamento"=> [
                 [
                     "servico_id"=> 6,
                     "quantidade"=> 1
@@ -53,15 +45,7 @@ class OrcamentoCest
         "descricao"=> "dfsdffsd",
         "preco"=> 250,
         "data_entrega"=> "2024-05-20",  
-        "fatura"=> null,
-        "utilizador_id"=> 3,   
-        "laboratorio_id"=> 1,        
-        "servicoOrcamentos"=> [
-                [
-                    "servico_id"=> 6,
-                    "quantidade"=> 1
-                ]
-            ]            
+        "laboratorio_id"=> 1                 
         ];
     public function testCreateOrcamento(FunctionalTester $I)
     {
@@ -73,7 +57,15 @@ class OrcamentoCest
 
         $I->seeResponseCodeIs(200);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseJsonMatchesJsonPath('$.descricao');
+        $I->seeResponseJsonMatchesJsonPath('$.preco');
+        $I->seeResponseJsonMatchesJsonPath('$.laboratorio_id');
+        $I->seeResponseJsonMatchesJsonPath('$.servicoOrcamento[0].servico_id');
+        $I->seeResponseJsonMatchesJsonPath('$.servicoOrcamento[0].quantidade');
+        $I->seeResponseJsonMatchesJsonPath('$.estadoOrcamento[0].estado_id');
+
+        
+        
     }
     public function testCreateOrcamentoSemAutorizacao(FunctionalTester $I)
     {
@@ -85,31 +77,61 @@ class OrcamentoCest
 
         $I->seeResponseCodeIs(401);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            "name"=> "Unauthorized",
+            "message"=> "Your request was made with invalid credentials.",
+            "status"=> 401
+        ]);
+    }
+    public function testCreateOrcamentoComTokenErrado(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+
+        $body = $this->body_to_create;
+        
+        $I->sendPOST('/api/orcamento', $body);
+
+        $I->seeResponseCodeIs(403);
+
+        $I->seeResponseContainsJson([
+            "name"=> "Forbidden",
+            "message"=> "Não tem permissão para criar orçamentos.",
+            "status"=> 403
+        ]);
     }
     public function testCreateOrcamentoBodyErrado(FunctionalTester $I)
     {
         $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
 
-        $body = $this->body_errado;
+        $body = $this->body_errado_to_create;
         
         $I->sendPOST('/api/orcamento', $body);
 
         $I->seeResponseCodeIs(400);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            "name"=> "Bad Request",
+            "message"=> "Faltam campos obrigatórios. descrição ou servicoOrcamento.",
+            "status"=> 400
+        ]);
     }
     public function testUpdateOrcamento(FunctionalTester $I)
     {
-        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
 
         $body = $this->body_to_update;
         $id = $this->id;
-        $I->sendPUT('/api/orcamento/'. $id, $body);
+        $I->sendPUT('/api/orcamento/update?idOrcamento=' . $id, $body);
 
         $I->seeResponseCodeIs(200);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseJsonMatchesJsonPath('$.descricao');
+        $I->seeResponseJsonMatchesJsonPath('$.preco');
+        $I->seeResponseJsonMatchesJsonPath('$.laboratorio_id');
+        $I->seeResponseJsonMatchesJsonPath('$.servicoOrcamento[0].servico_id');
+        $I->seeResponseJsonMatchesJsonPath('$.servicoOrcamento[0].quantidade');
+        $I->seeResponseJsonMatchesJsonPath('$.estadoOrcamento[0].estado_id');
+
     }
     public function testUpdateOrcamentoSemAutorizacao(FunctionalTester $I)
     {
@@ -117,34 +139,156 @@ class OrcamentoCest
 
         $body = $this->body_to_update;
         $id = $this->id;
-        $I->sendPUT('/api/orcamento/'. $id, $body);
+        $I->sendPUT('/api/orcamento/update?idOrcamento=' . $id, $body);
 
         $I->seeResponseCodeIs(401);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            "name"=> "Unauthorized",
+            "message"=> "Your request was made with invalid credentials.",
+            "status"=> 401
+        ]);
+    }
+    public function testUpdateOrcamentoComTokenErradoUsuario(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
+
+        $body = $this->body_to_update;
+        $id = 3;
+        $I->sendPUT('/api/orcamento/update?idOrcamento=' . $id, $body);
+
+        $I->seeResponseCodeIs(403);
+
+        $I->seeResponseContainsJson([
+            "name"=> "Forbidden",
+            "message"=> "Não tem permissão para atualizar este orçamento.",
+            "status"=> 403
+        ]);
+    }
+    public function testUpdateOrcamentoComTokenErradoLaboratorio(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_laboratorio);
+
+        $body = $this->body_to_update;
+        $id = 3;
+        $I->sendPUT('/api/orcamento/update?idOrcamento=' . $id, $body);
+
+        $I->seeResponseCodeIs(403);
+
+        $I->seeResponseContainsJson([
+            "name"=> "Forbidden",
+            "message"=> "Não tem permissão para atualizar este orçamento.",
+            "status"=> 403
+        ]);
     }
     public function testUpdateOrcamentoBodyErrado(FunctionalTester $I)
     {
-        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
 
         $body = $this->body_to_update_errado;
-        $id = $this->id;
-        $I->sendPUT('/api/orcamento/'. $id, $body);
+        $idOrcamento = $this->id;
+        $I->sendPUT('/api/orcamento/'. $idOrcamento, $body);
 
-        $I->seeResponseCodeIs(500);
+        $I->seeResponseCodeIs(400);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson([
+            "name"=> "Bad Request",
+            "message"=> "Missing required parameters: idOrcamento",
+            "status"=> 400
+        ]);
     }
-    public function testUpdateOrcamentoIdErrado(FunctionalTester $I)
+    public function testGetOrcamentoGabinete(FunctionalTester $I)
     {
         $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
 
-        $body = $this->body_to_update;
-        $id = $this->id;
-        $I->sendPUT('/api/orcamento/30', $body);
+        $I->sendGET('/api/orcamento/');
 
-        $I->seeResponseCodeIs(404);
+        $I->seeResponseCodeIs(200);
 
-        $I->seeResponseIsJson();
+        $I->seeResponseJsonMatchesJsonPath('$[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrada');
+        $I->seeResponseJsonMatchesJsonPath('$[0].descricao');
+        $I->seeResponseJsonMatchesJsonPath('$[0].preco');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrega');
+        $I->seeResponseJsonMatchesJsonPath('$[0].fatura');
+        $I->seeResponseJsonMatchesJsonPath('$[0].utilizador_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].laboratorio_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].estado_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].data');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].servico_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].quantidade');
+
     }
+    public function testGetOrcamentoUsuario(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_comum);
+
+        $I->sendGET('/api/orcamento/');
+
+        $I->seeResponseCodeIs(200);
+
+        $I->seeResponseJsonMatchesJsonPath('$[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrada');
+        $I->seeResponseJsonMatchesJsonPath('$[0].descricao');
+        $I->seeResponseJsonMatchesJsonPath('$[0].preco');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrega');
+        $I->seeResponseJsonMatchesJsonPath('$[0].fatura');
+        $I->seeResponseJsonMatchesJsonPath('$[0].utilizador_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].laboratorio_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].estado_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].data');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].servico_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].quantidade');
+
+    }
+    public function testGetOrcamentoLaboratorio(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_laboratorio);
+
+        $I->sendGET('/api/orcamento/');
+
+        $I->seeResponseCodeIs(200);
+
+        $I->seeResponseJsonMatchesJsonPath('$[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrada');
+        $I->seeResponseJsonMatchesJsonPath('$[0].descricao');
+        $I->seeResponseJsonMatchesJsonPath('$[0].preco');
+        $I->seeResponseJsonMatchesJsonPath('$[0].data_entrega');
+        $I->seeResponseJsonMatchesJsonPath('$[0].fatura');
+        $I->seeResponseJsonMatchesJsonPath('$[0].utilizador_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].laboratorio_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].estado_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].estadoOrcamento[0].data');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].orcamento_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].servico_id');
+        $I->seeResponseJsonMatchesJsonPath('$[0].servicoOrcamento[0].quantidade');
+
+    }
+    public function testGetOrcamentoSemToken(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer teste');
+
+        $I->sendGET('/api/orcamento/');
+
+        $I->seeResponseCodeIs(401);
+
+        $I->seeResponseContainsJson([
+            "name"=> "Unauthorized",
+            "message"=> "Your request was made with invalid credentials.",
+            "status"=> 401
+        ]);
+
+    }
+    
 }
