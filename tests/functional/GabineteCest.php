0e40141f6b1f09503c67ceb74a59a32232ca9398
a58790 <a58790@alunos.ipb.pt>
2024-10-26 18:42:36 +0100

    Criado function testGetUsers - Listar usuários

---
+++ tests/functional/GabineteCest.php
@@ -0,0 +1,50 @@
+<?php
+
+class GabineteCest
+{
+    // Propriedades da classe para armazenar os tokens
+    public $token_user_admin = "wK9DpRsBpCPjxyBTzkJ1fBU2-pbpaTyq";
+
+    public function testGetUsers(FunctionalTester $I)
+    {
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $this->token_user_admin);
+
+        $I->sendGET('/api/user/');
+
+        // Verificar se a resposta é um array
+        $I->seeResponseIsJson();
+
+        // Verificar se o status da resposta é 200
+        $I->seeResponseCodeIs(200);
+
+        // Verificar se contém pelo menos um usuário
+        $I->seeResponseJsonMatchesJsonPath('$[*]'); // Verifica que existem múltiplos usuários
+
+        // Decodificar a resposta JSON
+        $users = json_decode($I->grabResponse(), true);
+
+        // Verifica se a resposta é um array e contém pelo menos um usuário
+        $I->assertIsArray($users);
+        $I->assertNotEmpty($users);
+
+        // Verificar os campos esperados para cada usuário
+        foreach ($users as $user) {
+            $I->assertArrayHasKey('id', $user);
+            $I->assertArrayHasKey('role_id', $user);
+            $I->assertArrayHasKey('status', $user);
+            $I->assertArrayHasKey('email', $user);
+            $I->assertArrayHasKey('username', $user);
+            $I->assertArrayHasKey('password', $user);
+            $I->assertArrayHasKey('auth_key', $user);
+            $I->assertArrayHasKey('access_token', $user);
+            $I->assertArrayHasKey('logged_in_ip', $user);
+            $I->assertArrayHasKey('logged_in_at', $user);
+            $I->assertArrayHasKey('created_ip', $user);
+            $I->assertArrayHasKey('created_at', $user);
+            $I->assertArrayHasKey('updated_at', $user);
+            $I->assertArrayHasKey('banned_at', $user);
+            $I->assertArrayHasKey('banned_reason', $user);
+            $I->assertArrayHasKey('utilizador', $user);
+        }
+    } 
+}
